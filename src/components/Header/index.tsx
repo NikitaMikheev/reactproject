5f01f3b9947c54f5f22b1541814357f2e0725bd2
Nikita Mikheev <nikita.miheev.1999@mail.ru>
2023-05-14 15:24:56 +0300

    add hiddenHeader

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Logotype } from "../Logo";
 import { Time } from "./TimePic";
 import { MainMenu } from "../MainMenu";
 import { Button } from "../Button";
 import styles from "./Header.module.scss"
 import * as stylesBtn from "../Button/Button.module.scss"
+import { useDocumentScrollThrottled } from "./docScroll";
 
 export const Header = () => {
     
@@ -12,17 +13,32 @@ export const Header = () => {
         type: stylesBtn.default.primary,
         color: stylesBtn.default.orange
     };
+    const [shouldHideHeader, setShouldHideHeader] = useState(false);
+
+    const MINIMUM_SCROLL = 80;
+
+    useDocumentScrollThrottled(callbackData => {
+        const { currentScrollTop } = callbackData;
+        const isMinimumScrolled = currentScrollTop > MINIMUM_SCROLL;
     
+        setShouldHideHeader(isMinimumScrolled);
+      });
+
+
+    const hiddenBot= shouldHideHeader ? styles.hiddenBot : '';
+    const hiddenTop= shouldHideHeader ? styles.hiddenTop : '';
+
+
     return <>
-    <header className={styles.header}>
-        <div className={styles.header__top}>
+    <header className={`${styles.header}`}>
+        <div className={`${styles.header__top} ${hiddenTop}`}>
             <div className={`${styles.container} ${styles.flex}`}>
                 <Logotype />
                 <MainMenu />
             </div>
         </div>
 
-        <div className={styles.header__bot}>
+        <div className={`${styles.header__bot} ${hiddenBot}`}>
             <div className={`${styles.container} ${styles.flex}`}>
                 <div className={`${styles.header__worktime} ${styles.flex}`}>
                    <Time />
@@ -30,8 +46,8 @@ export const Header = () => {
                 </div>
 
                 <div className="header__feedback">
-                    <p className={styles.header_}>
-                        Позвоните нам: <a className="phone" href="tel:+79960004574"><br />+7 (996) 000-45-74</a>
+                    <p className={styles.header__botText}>
+                        Позвоните нам: <a className="phone" href="tel:+79960004574">+7 (996) 000-45-74</a>
                     </p>
                 </div>
 
